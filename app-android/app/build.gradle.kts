9574f1940c95a60335bff9e46438725c4d890c6f
Fernando Rivas <frivasm@miumg.edu.gt>
2025-11-16 21:18:52 -0600

    Nuevo Diseño Landing Page

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "org.manosquehablan"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
